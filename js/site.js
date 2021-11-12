d5bb948857dd1e6d5bab1edaba0472098ab24784
Brian Wiggins <irdetection@hotmail.com>
2021-11-12 17:38:08 -0500

    HTML completed Rewind Coding Challenge

--- js/site.js
+++ js/site.js
@@ -1,7 +1,42 @@
-// Prism JS Setup
+// Rewind Javascript
 
-//Call Hello
-function HelloWorld(){
-    alert("Hello World");
+//Call function
+let runRewind=document.getElementById("btnSubmit");
+runRewind.addEventListener("click", StartRewind);
+
+//Main Function
+function StartRewind(){
+	
+	//Get input values
+	let inputText=document.getElementById("inputText").value;
+
+	//Validate text entry to assure there is at least 1 character
+	if(inputText.length===0){
+		errorMessage="Please put in at least one character.";
+		let errorMessageDisplay=document.getElementById("validation-summary");
+		errorMessageDisplay.innerHTML=errorMessage;
+	}
+	
+	//Call Reverse String function
+	let reveresedString=GetReverseString(inputText);
+	
+	//Place on screen
+	ResultToScreen(reveresedString);
 }
 
+// function to reverse string
+function GetReverseString(inputText){
+	let reversedString=[];
+	for(let i=inputText.length-1;i>=0 ;i--){
+		reversedString.push(inputText[i]);
+	}
+	
+	return reversedString;
+}
+
+//Place result of character array to screen
+function ResultToScreen(outputText){
+    let fullString=outputText.join("");
+	let outputField=document.getElementById("outputTextField");
+	outputField.innerHTML=fullString;
+}
